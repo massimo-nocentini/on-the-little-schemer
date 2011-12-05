50bdedb99aff69e63784eb00d18456ba4d811640
Massimo Nocentini <massimo.nocentini@gmail.com>
2011-12-05 06:55:40 +0100

    some refactoring and comments

--- land-of-lisp/wumpus.lisp
+++ land-of-lisp/wumpus.lisp
@@ -1,3 +1,4 @@
+
 (load "dot-file-generator.lisp")
 
 (defparameter *congestion-city-nodes* nil)
@@ -291,3 +292,18 @@
 			 (when (some (function cdr) (cdr (assoc n edge-alist)))
 			   '(sirens!))))))
 			       
+(defun new-game ()
+  (setf *congestion-city-edges* (make-city-edges))
+  (setf *congestion-city-nodes* (make-city-nodes *congestion-city-edges*))
+  (setf *player-pos* (find-empty-node))
+  (setf *visited-nodes* (list *player-pos*))
+  (draw-city))
+
+(defun find-empty-node ()
+  (let ((x (random-node)))
+    (if (cdr (assoc x *congestion-city-nodes*))
+	(find-empty-node)
+	x)))
+
+(defun draw-city ()
+  (ugraph->png "city" *congestion-city-nodes* *congestion-city-edges*))
