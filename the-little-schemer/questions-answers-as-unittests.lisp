5d5ff95556b83d56b2b5f2181f30e0b675d77df7
Massimo Nocentini <massimo.nocentini@gmail.com>
2012-02-25 18:57:36 +0100

    Finished to write unit test version of the question on the first chapter.

--- the-little-schemer/questions-answers-as-unittests.lisp
+++ the-little-schemer/questions-answers-as-unittests.lisp
@@ -81,6 +81,10 @@
    '(b c)
    (cdr '(a b c)) )
 
+  (lisp-unit:assert-false
+   (eq '(b c)
+       (cdr '(a b c))) )  
+
   (lisp-unit:assert-equal
    '(x y z)
    (cdr '((a b c) x y z)) )  
@@ -108,7 +112,168 @@
    (equal '(r t)
    (cdr '((x) t r))) )
 
+  (lisp-unit:assert-error
+   'type-error
+   (cdr 'hotdogs))
+  
+  (lisp-unit:assert-eq
+   '()
+   (cdr '() ) )
 
+  ;; this assertion is the prelude of the next one.
+  (lisp-unit:assert-eq
+   '(x y)
+   '(x y))
   
-  
+  ;; this is quite strange because in a first place we apply cdr which
+  ;; build a new cons (as we observed some assertion above) and after
+  ;; we apply the car on the temporary list, and the two list are
+  ;; equal and equivalent, I expected only equivalent. After some
+  ;; thoughts, only the conses are built from scratch, but their
+  ;; content is the same, and the content is what is returned by the
+  ;; car, that is not the cons structure which, in turn, is returned
+  ;; by the cons application.  We've used the assert-eq because of the
+  ;; application of the car, which return the content, so we are in
+  ;; the case of the assertion above.
+  (lisp-unit:assert-eq
+   '(x y)
+   (car (cdr '((b) (x y) ((c))) )) )
+
+  (lisp-unit:assert-equal
+   '(((c)))
+   (cdr (cdr '((b) (x y) ((c))) )) )
+
+  (lisp-unit:assert-false
+   (eq '(((c)))
+       (cdr (cdr '((b) (x y) ((c))) ))) )
+
+  (lisp-unit:assert-error
+   'type-error				;to get this atom I've first
+					;simulated the following
+					;expression in the REPL
+   (cdr (car '(a (b (c)) d) ) ))
+    
+  )
+
+(lisp-unit:define-test cons-questions-chapter-one
+  (lisp-unit:assert-equal		;we have to use the equal
+					;predicate because cons return
+					;a structure not the content
+					;(like car does)
+   '(peanut butter and jelly)
+   ;; read the following one as "cons the atom 'peanut onto the list"
+   (cons 'peanut '(butter and jelly)) )
+
+  (lisp-unit:assert-equal
+   '((banana and) peanut butter and jelly)
+   (cons '(banana and) '(peanut butter and jelly)))
+
+  (lisp-unit:assert-equal
+   '(((help) this) is very ((hard) to learn))
+   (cons '((help) this) '(is very ((hard) to learn))))
+
+  (lisp-unit:assert-equal
+   '((a b (c)))
+   (cons '(a b (c)) ()) )
+
+  (lisp-unit:assert-equal
+   '(a)
+   (cons 'a ()))
+
+  (lisp-unit:assert-equal		;also for the 'dotted' cons we
+					;have to use the equal
+					;predicate instead of eq,
+					;because using cons for this
+					;special dotted case, the
+					;structure is what is
+					;returned, not the content,
+					;that is an S-expression
+   '(((a b c)) . b)
+   (cons '((a b c)) 'b))
+
+  (lisp-unit:assert-equal
+   '(a . b)
+   (cons 'a 'b))
+
+  (lisp-unit:assert-eq
+   'a
+   (car (cons 'a 'b)))
+
+  (lisp-unit:assert-eq			;in this case we can use the
+					;eq predicate because the cons
+					;application return an
+					;S-expression, not a cons
+					;structure like when we handle
+					;a stardard list (not a
+					;'dotted' cons, which is not a
+					;list)
+   'b
+   (cdr (cons 'a 'b)))
+
+  (lisp-unit:assert-equal
+   '(a b)
+   (cons 'a (car '((b) c d))))
+
+  (lisp-unit:assert-equal
+   '(a c d)
+   (cons 'a (cdr '((b) c d))))
+  )
+
+(lisp-unit:define-test null-questions-chapter-one
+  (lisp-unit:assert-true
+   (null (quote ())) )
+
+  (lisp-unit:assert-false
+   (null '(a b c)))
+
+  (lisp-unit:assert-false
+   (null 'spaghetti))
+
+  (lisp-unit:assert-true
+   (atomp 'Harry))
+
+  (lisp-unit:assert-false
+   (atomp '(Harry had a heap of apples)))
+
+  (lisp-unit:assert-true
+   (atomp (car '(Harry had a heap of apples))))
+
+  (lisp-unit:assert-false
+   (atomp (cdr '(Harry had a heap of apples))))
+
+  (lisp-unit:assert-false
+   (atomp (cdr '(Harry))))
+
+  (lisp-unit:assert-true
+   (atomp (car (cdr '(swing low sweet cherry oat)))))
+
+  (lisp-unit:assert-false
+   (atomp (car (cdr '(swing (low sweet) cherry oat)))))
+
+  (lisp-unit:assert-eq
+   'Harry
+   'Harry)
+
+  (lisp-unit:assert-false
+   (eq 'margarine
+       'butter))
+
+  (lisp-unit:assert-false
+   (eq () '(strawberry)))
+
+  (lisp-unit:assert-false
+   (eq 6
+       7))
+
+  (lisp-unit:assert-eq
+   'Mary
+   (car '(Mary had a little lamb chop)))
+
+  (lisp-unit:assert-false
+   (eq (cdr '(soured milk))
+       'milk))
+
+  (lisp-unit:assert-eq
+   (car '(beans beans we need jelly beans))
+   (car (cdr '(beans beans we need jelly beans))))
   )
