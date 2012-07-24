ef6feee2dfe011a08f0587a044d66853a6a456e5
Massimo Nocentini <massimo.nocentini@gmail.com>
2012-07-24 16:26:21 +0200

    Add and test function COVERED?

--- the-little-schemer/exercises-logic-expressions-unittests.lisp
+++ the-little-schemer/exercises-logic-expressions-unittests.lisp
@@ -10,6 +10,16 @@
   (lisp-unit:assert-false (lexp? '(NOT x (AND u i))))
   (lisp-unit:assert-false (lexp? '(1 + (3 x 4))))
   (lisp-unit:assert-false (lexp? '(3 + (66 6))))
+
+  (lisp-unit:assert-true (covered?
+			  '()
+			  '(x y o y u i)))  
+  (lisp-unit:assert-true (covered?
+			  '(and (or x y o) y u (not i))
+			  '(x y o u i)))
+  (lisp-unit:assert-false (covered?	;Y missing
+			  '(and (or x y o) y u (not i))
+			  '(x o u i)))
   
 
 
--- the-little-schemer/exercises-logic-expressions.lisp
+++ the-little-schemer/exercises-logic-expressions.lisp
@@ -10,7 +10,9 @@
 					      ;allow illimited logic
 					      ;expression arguments
 	 (eq 'or (prefix-operator-getter l)))
-     (unlimited-arguments-checker (and-or-arguments-getter l)))
+     (unlimited-arguments-checker
+      (and-or-arguments-getter l)
+      #'lexp?))
     ((and (eq 2 (length l))		;for the NOT operator we
 					;require exactly one
 					;sub-logic-exp
@@ -25,18 +27,61 @@
 					;sexp (which can be an atom or
 					;a list of course)
 
-(defun unlimited-arguments-checker (args)
+(defun unlimited-arguments-checker (args mutual-recursion)
   (cond
     ((null args) t)			;all the arguments are
 					;well-formed logic expressions
-    (t (and (lexp? (car args))		;make mutual recursion with
+    (t (and (funcall mutual-recursion
+		     (car args))	;make mutual recursion with
 					;lexp? followed by natural
 					;recursion on the remaining
 					;arguments.
-	    (unlimited-arguments-checker (cdr args))))))
+	    (unlimited-arguments-checker (cdr args)
+					 mutual-recursion)))))
 
 (defun prefix-operator-getter (l)
   (car l))
 
 (defun and-or-arguments-getter (l)
   (cdr l))
+
+(defun covered? (lexp los)
+  "Assume that lexp is a valid logic expression"
+  (cond
+    ((null lexp) t)
+    ((atomp lexp) (memberp lexp los))
+    ((or (eq 'and (prefix-operator-getter lexp)) ;for AND and OR op we
+					      ;allow illimited logic
+					      ;expression arguments
+	 (eq 'or (prefix-operator-getter lexp)))
+     (unlimited-arguments-checker
+      (and-or-arguments-getter lexp)
+      #'(lambda (argument-lexp)		;here we have to pass this
+					;lambda because we care about
+					;the LOS argument because is
+					;necessary for the application
+					;of covered? to the argument
+					;of the lambda (otherwise we
+					;could have passed LOS to the
+					;function
+					;UNLIMITED-ARGUMENTS-CHECKER,
+					;but that new argument is
+					;relative to only the function
+					;covered?, for the function
+					;lexp? is meaningless to pass
+					;it: this help to keep the
+					;signature of
+					;UNLIMITED-ARGUMENTS-CHECKER
+					;clean and short.
+	  (covered? argument-lexp los))))
+    (t (covered?		       ;since we assume that LEXP is a
+				       ;valid logic expression, if the
+				       ;preceeding cond-line fail,
+				       ;here the operator have to be
+				       ;NOT
+	(not-arguments-getter lexp) los)))) ;just recur with the
+					    ;single sub expression
+					    ;tocheck
+ 
+    
+  
