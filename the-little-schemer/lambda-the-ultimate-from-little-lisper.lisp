177639b730086aa0ce7504db6b355f9a71c228f9
Massimo Nocentini <massimo.nocentini@gmail.com>
2012-07-29 16:35:09 +0200

    Try to refactor the function VALUE in a different manner, arriving to the same result showed in the book.

--- the-little-schemer/lambda-the-ultimate-from-little-lisper.lisp
+++ the-little-schemer/lambda-the-ultimate-from-little-lisper.lisp
@@ -72,6 +72,140 @@
 			   #'equal)
 	       nil a l)))
 
+(defun value-with-repetition (aexp)
+  "Assume AEXP an arithmetic expression and the only operator that can
+appear in it are the + and * operator."
+  (cond
+    ((numberp aexp) aexp)
+    ((eq '+ (car aexp)) (+ (value (cadr aexp))
+			   (value (caddr aexp))))
+    ((eq '* (car aexp)) (* (value (cadr aexp))
+			   (value (caddr aexp))))))
+
+(defun value-with-encapsulated-common-behavior (aexp)
+  "Assume AEXP an arithmetic expression and the only operator that can
+appear in it are the + and * operator."
+  (cond
+    ((numberp aexp) aexp)
+    ((eq '+ (car aexp)) ((lambda (op-fun)
+			   (funcall op-fun
+				    (value (cadr aexp))
+				    (value (caddr aexp)))) #'+))
+    ((eq '* (car aexp)) ((lambda (op-fun)
+			   (funcall op-fun
+				    (value (cadr aexp))
+				    (value (caddr aexp)))) #'*))))
+
+(defun value-with-abstracted-common-behavior (aexp)
+  "Assume AEXP an arithmetic expression and the only operator that can
+appear in it are the + and * operator."
+  ((lambda (apply-with-operator)
+     (cond
+       ((numberp aexp) aexp)
+       ((eq '+ (car aexp)) (funcall apply-with-operator #'+))
+       ((eq '* (car aexp)) (funcall apply-with-operator #'*))))
+   (lambda (op-fun) (funcall op-fun
+			     (value (cadr aexp))
+			     (value (caddr aexp))))))
+
+(defun value-with-simplified-cond-expr (aexp)
+  "Assume AEXP an arithmetic expression and the only operator that can
+appear in it are the + and * operator."
+  ((lambda (apply-with-operator recfun)
+     (cond
+       ((numberp aexp) aexp)
+       (t (or (funcall recfun '+ (car aexp) #'+ apply-with-operator)
+	      (funcall recfun '* (car aexp) #'* apply-with-operator)))))
+   (lambda (op-fun) (funcall op-fun
+			     (value (cadr aexp))
+			     (value (caddr aexp))))
+   (lambda (op-symbol operator operator-f apply-with-operator)
+	   (when (eq op-symbol operator)
+	     (funcall apply-with-operator operator-f)))))
+
+(defun value-with-simplified-inner-lambdas (aexp)
+  "Assume AEXP an arithmetic expression and the only operator that can
+appear in it are the + and * operator."
+  ((lambda (recfun)
+     (cond
+       ((numberp aexp) aexp)
+       (t (or (funcall recfun '+ (car aexp) #'+ )
+	      (funcall recfun '* (car aexp) #'* )))))
+   (lambda (op-symbol operator operator-f)
+     (when (eq op-symbol operator)
+       ((lambda (op-fun) (funcall op-fun
+				  (value (cadr aexp))
+				  (value (caddr aexp)))) operator-f)))))
+
+(defun value-with-prepared-op-function-builder (aexp)
+  "Assume AEXP an arithmetic expression and the only operator that can
+appear in it are the + and * operator."
+  ((lambda (recfun)
+     (cond
+       ((numberp aexp) aexp)
+       (t (or (funcall recfun
+		       ((lambda (operator)
+			  (cond
+			    ((eq '+ operator) #'+)
+			    ((eq '* operator) #'*))) (car aexp)))
+	      (funcall recfun
+ 		       ((lambda (operator)
+			  (cond
+			    ((eq '+ operator) #'+)
+			    ((eq '* operator) #'*))) (car aexp)))))))
+   (lambda (operator-f)
+     (funcall operator-f
+	      (value (cadr aexp))
+	      (value (caddr aexp))))))
+
+(defun value-with-doubled-or-branches (aexp)
+  "Assume AEXP an arithmetic expression and the only operator that can
+appear in it are the + and * operator."
+  ((lambda (recfun op-to-op-f)
+     (cond
+       ((numberp aexp) aexp)
+       (t (or (funcall recfun (funcall op-to-op-f (car aexp)))
+	      (funcall recfun (funcall op-to-op-f (car aexp)))))))
+   (lambda (operator-f)
+     (funcall operator-f
+	      (value (cadr aexp))
+	      (value (caddr aexp))))
+   (lambda (operator)
+     (cond
+       ((eq '+ operator) #'+)
+       ((eq '* operator) #'*)))))
+
+(defun value-prepared-for-reclambda-inlining (aexp)
+  "Assume AEXP an arithmetic expression and the only operator that can
+appear in it are the + and * operator."
+  ((lambda (recfun op-to-op-f)
+     (cond
+       ((numberp aexp) aexp)
+       (t (funcall recfun (funcall op-to-op-f (car aexp))))))
+   (lambda (operator-f)
+     (funcall operator-f
+	      (value (cadr aexp))
+	      (value (caddr aexp))))
+   (lambda (operator)
+     (cond
+       ((eq '+ operator) #'+)
+       ((eq '* operator) #'*)))))
+
+(defun value (aexp)
+  "Assume AEXP an arithmetic expression and the only operator that can
+appear in it are the + and * operator."
+  ((lambda (op-to-op-f)
+     (cond
+       ((numberp aexp) aexp)
+       (t (funcall (funcall op-to-op-f (car aexp))
+		   (value (cadr aexp))
+		   (value (caddr aexp))))))
+   (lambda (operator)
+     (cond
+       ((eq '+ operator) #'+)
+       ((eq '* operator) #'*)))))
+
+
 (define-test lambda-the-ultimate-from-little-lisper
 
   (assert-equal '(6 2 3)
@@ -111,4 +245,10 @@
   (assert-true
    '(salad something else)
    (REMBER-F-generate-something-that-INSERT-GEN-can-generate?
-    'old-atom '(salad old-atom something else))) )
+    'old-atom '(salad old-atom something else)))
+
+  (assert-eq 4 (value '(+ 1 3)))
+  (assert-eq 16 (value '(+ 1 (* 3 5))))
+
+  )
+
